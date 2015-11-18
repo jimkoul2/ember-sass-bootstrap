4e5318f120f186b9bb550f241b740bbff1fe58a8
Dimitris <koulmitsakos@gmail.com>
2015-11-18 12:28:17 +0200

    First Commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    sassOptions: {
+        includePaths: [
+          'bower_components/bootstrap-sass/assets/stylesheets'
+        ]
+      }
     // Add options here
   });
 
@@ -18,6 +23,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
+  app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
   return app.toTree();
 };
